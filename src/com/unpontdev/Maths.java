7aa20934fc358a984daa61b40d2d570f1b04dd36
cjrocha <Marateodora2005>
2021-12-10 21:27:18 +0200

    Second commit

--- src/com/unpontdev/Maths.java
+++ src/com/unpontdev/Maths.java
@@ -7,11 +7,12 @@ package com.unpontdev;
 
 public class Maths {
     public static void main(String[] args) {
-        int no = 100, sum = 0;
+        int sum = 0;
         for (int i = 1; i <= 100; ++i) {
             //adding i to sum
             sum += i;
         }
         System.out.println("Sum is = " + sum);
     }
+
 }
